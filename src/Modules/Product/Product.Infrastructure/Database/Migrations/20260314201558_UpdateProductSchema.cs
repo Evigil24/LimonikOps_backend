413372898282b38c4074a11736b0dbc0693bfdcd
Evigil24 <bvigil_now@hotmail.com>
2026-03-14 14:40:23 -0600

    feat(dotnet): add dotnet-ef tool version 10.0.3 for database migrations

---
+++ src/Modules/Product/Product.Infrastructure/Database/Migrations/20260314201558_UpdateProductSchema.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LimonikOne.Modules.Product.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateProductSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder) { }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder) { }
+    }
+}
